672c67f47126132dfaba569e985ddb369ed5afec
Becca <34410968+rtviner@users.noreply.github.com>
2026-05-14 16:50:03 -0600

    update authentication controller to use tokens not cookies
    fixup! update authentication controller to use tokens not cookies

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -3,7 +3,7 @@ module Authentication
 
   included do
     before_action :require_authentication
-    helper_method :authenticated?
+    # helper_method :authenticated?
   end
 
   class_methods do
@@ -13,46 +13,53 @@ module Authentication
   end
 
   private
-    def authenticated?
-      resume_session
-    end
+    # def authenticated?
+    #   resume_session
+    # end
 
     def require_authentication
-      resume_session || request_authentication
+      # resume_session || request_authentication
+      resume_session || render_unauthorized
     end
 
     def resume_session
-      Current.session ||= find_session_by_token || find_session_by_cookie
+      # Current.session ||= find_session_by_token || find_session_by_cookie
+      Current.session ||= find_session_by_token
     end
 
     def find_session_by_token
       authenticate_with_http_token do |token, options|
-        Session.find_by(token: token)
+        Session.find_by(auth_token: token)
       end
     end
+    # ! add cookie related code back once frontend is created and can handle cookies properly
+    # def find_session_by_cookie
+    #   Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
+    # end
 
-    def find_session_by_cookie
-      Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
+    # def request_authentication
+    #   session[:return_to_after_authenticating] = request.url
+    #   redirect_to new_session_path
+    # end
+    #
+
+    def render_unauthorized
+      render json: { error: "Unauthorized" }, status: :unauthorized
     end
 
-    def request_authentication
-      session[:return_to_after_authenticating] = request.url
-      redirect_to new_session_path
-    end
-
-    def after_authentication_url
-      session.delete(:return_to_after_authenticating) || root_url
-    end
+    # def after_authentication_url
+    #   session.delete(:return_to_after_authenticating) || root_url
+    # end
 
     def start_new_session_for(user)
       user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
         Current.session = session
-        cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
+        # cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
       end
     end
 
     def terminate_session
       Current.session.destroy
-      cookies.delete(:session_id)
+      # cookies.delete(:session_id)
     end
 end
