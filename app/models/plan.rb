27547f8c4edab8b3ad711e9b76dd8b8b6dd80d30
Becca <34410968+rtviner@users.noreply.github.com>
2026-06-01 14:05:12 -0600

    use progression for constant instead of buld

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,13 +1,13 @@
 class Plan < ApplicationRecord
   belongs_to :user
   has_many :weeks, dependent: :destroy
-  MAX_BUILD_PERCENTAGE = 15.freeze
+  MAX_PROGRESSION_PERCENTAGE = 15.freeze
 
   enum :recovery_pattern, { every_other: 0, every_third: 1, every_fourth: 2 }
   enum :status, { planned: 0, active: 1, completed: 2, abandoned: 3 }
 
   validates :baseline_vertical_distance, :baseline_duration, :goal_vertical_distance, :recovery_pattern, :vertical_build_percentage, :status, presence: true
-  validates :vertical_build_percentage, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: MAX_BUILD_PERCENTAGE }
+  validates :vertical_build_percentage, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: MAX_PROGRESSION_PERCENTAGE }
   validates :goal_vertical_distance, numericality: { greater_than: :baseline_vertical_distance }
   validate :end_date_after_start_date, if: -> { start_date.present? && end_date.present? }
 
